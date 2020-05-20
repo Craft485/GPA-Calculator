adb5fdc53303a59008599c0b4790553a2c5ef0a1
Craft <38158429+Craft485@users.noreply.github.com>
2020-05-20 08:16:52 -0400

    used the same grade calc method as before

--- Calculator_BETA.js
+++ Calculator_BETA.js
@@ -33,21 +33,103 @@
             }
         }
         console.log("Found class levels: \n"+classLevels)
-        // calc()
+        calc()
     }
-    // function calc() {
-    //     for (i = 0; i < gradeArr.length; i++) {
-    //         let grade =gradeArr[i]
-    //         let level = classLevels[i]
-    //         switch (grade, level) {
-    //             case 100<=grade<=99:
-    //             case level 
-                    
-    //                 break;
-            
-    //             default:
-    //                 break;
-    //         }
-    //     }
-    // }
+    function calc() {
+        for (i = 0; i < gradeArr.length; i++) {
+            let gradePercent = gradeArr[i]
+            let classLvl = classLevels[i]
+            if (92 <= gradePercent && gradePercent <= 100) { //4.0+
+                if (classLvl === "AP") {
+                    additiveGPA += 4.8
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 4.4
+                } else if (classLvl === "CP") {
+                    additiveGPA += 4
+                }
+            } else if (90 <= gradePercent && gradePercent <= 91) { //3.8-4.56
+                if (classLvl === "AP") {
+                    additiveGPA += 4.56
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 4.18
+                } else if (classLvl === "CP") {
+                    additiveGPA += 3.8
+                }
+            } else if (88 <= gradePercent && gradePercent <= 89) { //3.2-3.84
+                if (classLvl === "AP") {
+                    additiveGPA += 3.84
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 3.52
+                } else if (classLvl === "CP") {
+                    additiveGPA += 3.2
+                }
+            } else if (82 <= gradePercent && gradePercent <= 87) { //3-3.6
+                if (classLvl === "AP") {
+                    additiveGPA += 3.6
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 3.3
+                } else if (classLvl === "CP") {
+                    additiveGPA += 3
+                }
+            } else if (80 <= gradePercent && gradePercent <= 81) { //2.8-3.36
+                if (classLvl === "AP") {
+                    additiveGPA += 3.36
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 3.08
+                } else if (classLvl === "CP") {
+                    additiveGPA += 2.8
+                }
+            } else if (78 <= gradePercent && gradePercent <= 79) { //2.2-2.64
+                if (classLvl === "AP") {
+                    additiveGPA += 2.64
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 2.42
+                } else if (classLvl === "CP") {
+                    additiveGPA +=2.2
+                }
+            } else if (72 <= gradePercent && gradePercent <= 77) { //2-2.4
+                if (classLvl === "AP") {
+                    additiveGPA += 2.4
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 2.2
+                } else if (classLvl === "CP") {
+                    additiveGPA += 2
+                }
+            } else if (70 <= gradePercent && gradePercent <= 71) { //1.8-2.16
+                if (classLvl === "AP") {
+                    additiveGPA += 2.16
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 1.98
+                } else if (classLvl === "CP") {
+                    additiveGPA += 1.8
+                }
+            } else if (68 <= gradePercent && gradePercent <= 69) { //1.2-1.44
+                if (classLvl === "AP") {
+                    additiveGPA += 1.44
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 1.32
+                } else if (classLvl === "CP") {
+                    additiveGPA += 1.2
+                }
+            } else if (62 <= gradePercent && gradePercent <= 67) { //1-1.2
+                if (classLvl === "AP") {
+                    additiveGPA += 1.2
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 1.1
+                } else if (classLvl === "CP") {
+                    additiveGPA += 1
+                }
+            } else if (60 <= gradePercent && gradePercent <= 61) { //0.8-0.96
+                if (classLvl === "AP") {
+                    additiveGPA += 0.96
+                } else if (classLvl === "HONORS") {
+                    additiveGPA += 0.88
+                } else if (classLvl === "CP") {
+                    additiveGPA += 0.8
+                }
+            } else if (gradePercent <= 59) {//F
+                additiveGPA += 0
+            }
+        }
+    }
 }()
